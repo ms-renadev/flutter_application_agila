aaa29061ddf56268f140e99512c977cb52337d3a
ms-rena-dev <renalynjoycebascones@gmail.com>
2026-03-25 02:31:29 +0800

    added in line 7 the parameters of signinwithemailandpassword

--- lib/auth/auth_service.dart
+++ lib/auth/auth_service.dart
@@ -1,5 +1,13 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AuthService {
-  final SupabaseClient
+  final SupabaseClient _supabase = Supabase.instance.client;
+
+  //sign in with email and password
+  Future<AuthResponse> signInWithEmailPassword
+  //sign up with email and password
+
+  //sign out 
+
+  //get user email
 }
