a3ff54d05c46be36d315f84fbc65995d9f344990
ms-rena-dev <renalynjoycebascones@gmail.com>
2026-03-25 02:40:09 +0800

    currently working on signUpWithEmailPassword function in auth_service.dart in auth folder

--- lib/auth/auth_service.dart
+++ lib/auth/auth_service.dart
@@ -4,9 +4,21 @@ class AuthService {
   final SupabaseClient _supabase = Supabase.instance.client;
 
   //sign in with email and password
-  Future<AuthResponse> signInWithEmailPassword
+  Future<AuthResponse> signInWithEmailPassword(
+    String email, String password) async{
+      return await _supabase.auth.signInWithPassword(
+        email: email,
+        password: password,
+      );
+  }
   //sign up with email and password
-
+  Future<AuthResponse> signUpWithEmailPassword(
+    String email, String password) async{
+      return await _supabase.auth.signUp(
+        email: email, 
+        password: password,
+      );
+  }
   //sign out 
 
   //get user email
