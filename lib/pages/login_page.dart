be3a7a03b267c36d47249346caea8cc4d250ce10
ms-rena-dev <renalynjoycebascones@gmail.com>
2026-03-26 17:07:30 +0800

    let us start building the UI

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_agila/auth/auth_service.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,6 +9,33 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  //get the auth service
+  final authService = AuthService();
+
+  //text controllers
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  //login button pressed
+  void login() async {
+    //prep the data
+    final email = _emailController.text;
+    final password = _passwordController;
+    //attempt the login
+    try{
+      await authService.signInWithEmailPassword(email, password);
+    }
+    //catch the errors if something goes wrong in login page
+    catch(e){
+      if(mounted){
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Error: $e")));
+      }
+    }
+
+  }
+
+  //build the UI
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
